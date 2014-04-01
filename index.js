d467f17e93c34fe36253d129171e594471ac1888
Matt Walters <mattwalters5@gmail.com>
2014-03-31 20:15:41 -0400

    adding debug statements

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var gm = require('gm')
-  , imageMagick = gm.subClass({ imageMagick: true });
+var debug = require('debug')('knowjs:resize'),
+		gm = require('gm'),
+		imageMagick = gm.subClass({ imageMagick: true });
 
 module.exports = function (path, width, height, cb) {
+	debug('resizing image %s to %s %s', path, width, height);
   imageMagick(path)
     .resize(width, height)
     .stream('png', function (err, stdout, stderr) {
       if (err) return cb(err);
+			debug('resized image %s', path);
       cb(null, stdout);  
     });
 }
